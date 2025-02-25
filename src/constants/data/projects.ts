12565c89330729dd46c85c9b363ba522e6e92fad
Zain🍕 <zainadam.id@gmail.com>
2025-02-26 02:41:14 +0800

    Improve `stacks` type

--- src/constants/data/projects.ts
+++ src/constants/data/projects.ts
@@ -3,7 +3,6 @@ import GitHubIcon from '@mui/icons-material/GitHub'
 import WebIcon from '@mui/icons-material/Language'
 //
 import type Project from '@/types/project'
-import type { Brand } from '@/types/brand'
 
 const projects: Project[] = [
     {
@@ -11,7 +10,7 @@ const projects: Project[] = [
         year: 2023,
         description:
             'Sensasi Logistic is an application designed for stock management of raw materials and finished products in production processes. The application allows users to efficiently track inventory, optimize supply chains, and eliminate production delays.',
-        stacks: ['laravel', 'alpinejs', 'bootstrap'] as Brand[],
+        stacks: ['laravel', 'alpinejs', 'bootstrap'],
         buttons: [
             {
                 text: 'Source Code',
@@ -29,7 +28,7 @@ const projects: Project[] = [
         year: 2022,
         description:
             'Application that designed to help palm oil farmers in East Kalimantan estimate their potential income based on the regulations set by palm oil companies',
-        stacks: ['react', 'material-ui'] as Brand[],
+        stacks: ['react', 'material-ui'],
         buttons: [
             {
                 text: 'Source Code',
@@ -48,7 +47,7 @@ const projects: Project[] = [
         year: 2022,
         description:
             'This project is a part of my final project for my master degree. This project is built using Hyperledger Fabric and Docker.',
-        stacks: ['hyperledger', 'docker'] as Brand[],
+        stacks: ['hyperledger', 'docker'],
         buttons: [
             {
                 text: 'Source Code',
@@ -62,7 +61,7 @@ const projects: Project[] = [
         year: 2022,
         description:
             'Material Blade is a simple package that provides the beautifulness of Google Material Design components as Laravel Blade components. Material Blade aims to make UI/UX development of your Laravel app faster, inspired by Material UI.',
-        stacks: ['laravel'] as Brand[],
+        stacks: ['laravel'],
         buttons: [
             {
                 text: 'Packagist',
@@ -75,7 +74,7 @@ const projects: Project[] = [
         year: 2023,
         description:
             'Sensasi Navbar is a Chrome extension designed to enhance navigation within the browser, particularly when Chrome is in full-screen mode (F11). It provides a navigation bar that assists in navigating through tabs, offering convenience and ease of use.',
-        stacks: ['react', 'material-ui', 'typescript'] as Brand[],
+        stacks: ['react', 'material-ui', 'typescript'],
         buttons: [
             {
                 text: 'Source Code',
@@ -90,7 +89,7 @@ const projects: Project[] = [
         year: 2024,
         description:
             'A simple Point of Sale application designed to help record sales of goods at stalls / shops / UMKM / stands / outlets / supermarkets.',
-        stacks: ['nextjs', 'hero-ui'] as Brand[],
+        stacks: ['nextjs', 'hero-ui'],
         buttons: [
             {
                 text: 'Source Code',
@@ -105,7 +104,7 @@ const projects: Project[] = [
         year: 2025,
         description:
             'A simple Point of Sale application designed to help record sales of goods at stalls / shops / UMKM / stands / outlets / supermarkets.',
-        stacks: ['react', 'material-ui'] as Brand[],
+        stacks: ['react', 'material-ui'],
         buttons: [
             {
                 text: 'Visit',
@@ -120,7 +119,7 @@ const projects: Project[] = [
         year: 2025,
         description:
             'Free and open source tool that is a web app that allows you to reformat/convert your file(s).',
-        stacks: ['remix', 'material-ui'] as Brand[],
+        stacks: ['remix', 'material-ui'],
         buttons: [
             {
                 text: 'Visit',
@@ -129,6 +128,6 @@ const projects: Project[] = [
             },
         ],
     },
-].sort((a, b) => b.year - a.year)
+]
 
-export default projects
+export default projects.sort((a, b) => b.year - a.year)
