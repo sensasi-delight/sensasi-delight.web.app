80b60cee837da196809140538e9993c50f536f74
Zain🍕 <zainadam.id@gmail.com>
2025-02-26 20:19:07 +0800

    Reduce code by using `<Section />`

--- src/components/sections/projects/index.tsx
+++ src/components/sections/projects/index.tsx
@@ -1,52 +1,35 @@
-// materials
-import Box from '@mui/material/Box'
 // components
 import ScrollableXBox from '@/components/scrollable-x-box'
 // data
 import projects from '@/constants/data/projects'
 import socials from '@/constants/data/socials'
 // parents components
-import SectionTitle from '@/components/sections/_components/layout/components/Title'
 import SocialIconButton from '@/components/sections/_components/social-icon-button'
 // sub
 import ProjectItemCard from './_components/project-item-card'
+import Section from '../_components/layout'
 
-const GitHub = socials.find(social => social.name === 'GitHub')
+const githubData = socials.find(social => social.name === 'GitHub')
 
 export default function ProjectsSection() {
     return (
-        <Box
+        <Section
             id="projects"
-            sx={{
-                transition: 'all 0.5s ease-in-out',
-            }}>
-            <Box
-                mb={2}
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    gap: 1,
-                    justifyContent: {
-                        xs: 'space-between',
-                        md: 'flex-start',
-                    },
-                }}>
-                <SectionTitle>Projects</SectionTitle>
-
-                {GitHub && (
+            title="Projects"
+            endTitle={
+                githubData && (
                     <SocialIconButton
-                        data={GitHub}
+                        data={githubData}
                         size="sm"
                         title="See more on my GitHub"
                     />
-                )}
-            </Box>
-
+                )
+            }>
             <ScrollableXBox>
                 {projects.map((project, i) => (
                     <ProjectItemCard key={i} data={project} />
                 ))}
             </ScrollableXBox>
-        </Box>
+        </Section>
     )
 }
--- src/components/sections/writings/index.tsx
+++ src/components/sections/writings/index.tsx
@@ -1,62 +1,50 @@
 // materials
 import Box from '@mui/material/Box'
 // components
-import SectionTitle from '@/components/sections/_components/layout/components/Title'
-import SocialIconButton from '@/components/sections/_components/social-icon-button'
+import ScrollableXBox from '@/components/scrollable-x-box'
 // sub-components
 import WritingCard from './_components/writing-card-item'
+// parents components
+import SocialIconButton from '../_components/social-icon-button'
+import Section from '../_components/layout'
 // data
 import socials from '@/constants/data/socials'
 import writings from '@/constants/data/writings'
 
-const Medium = socials.find(social => social.name === 'Medium')
-const GoogleScholar = socials.find(social => social.name === 'Google Scholar')
+const mediumData = socials.find(social => social.name === 'Medium')
+const googleScholarData = socials.find(
+    social => social.name === 'Google Scholar',
+)
 
 export default function WritingsSection() {
     return (
-        <Box id="writings">
-            <Box
-                mb={2}
-                sx={{
-                    display: 'flex',
-                    justifyContent: {
-                        xs: 'space-between',
-                        md: 'flex-start',
-                    },
-                    alignItems: 'center',
-                    gap: 2,
-                }}>
-                <SectionTitle>Writings</SectionTitle>
-
+        <Section
+            id="writings"
+            title="Writings"
+            endTitle={
                 <Box>
-                    {Medium && (
+                    {mediumData && (
                         <SocialIconButton
-                            data={Medium}
+                            data={mediumData}
                             size="sm"
                             title="Read more on my Medium"
                         />
                     )}
 
-                    {GoogleScholar && (
+                    {googleScholarData && (
                         <SocialIconButton
-                            data={GoogleScholar}
+                            data={googleScholarData}
                             size="sm"
                             title="Read more on my Google Scholar"
                         />
                     )}
                 </Box>
-            </Box>
-
-            <Box
-                display="flex"
-                gap={3}
-                sx={{
-                    overflowX: 'auto',
-                }}>
+            }>
+            <ScrollableXBox>
                 {writings.map((writing, i) => (
                     <WritingCard key={i} data={writing} />
                 ))}
-            </Box>
-        </Box>
+            </ScrollableXBox>
+        </Section>
     )
 }
