2b5888ec0a94f28e637408d90caed2eb20afcb3e
Zain🍕 <zainadam.id@gmail.com>
2025-02-26 20:05:34 +0800

    Improve customization

--- src/components/code-tag.tsx
+++ src/components/code-tag.tsx
@@ -1,17 +1,25 @@
 // vendors
 import type { ReactNode } from 'react'
 // materials
-import Typography from '@mui/material/Typography'
+import Typography, { type TypographyProps } from '@mui/material/Typography'
 
-export default function CodeTag({ children }: { children: ReactNode }) {
+export default function CodeTag({
+    children,
+    component = 'span',
+    fontSize = 'inherit',
+}: {
+    children: ReactNode
+    component?: TypographyProps['component']
+    fontSize?: TypographyProps['fontSize']
+}) {
     return (
         <Typography
             color="info.main"
-            component="span"
+            component={component}
+            fontSize={fontSize}
             sx={theme => ({
                 fontFamily: 'monospace',
                 fontWeight: 'bold',
-                fontSize: 'inherit',
                 '&:after': {
                     content: '">"',
                     color: theme.palette.text.secondary,
