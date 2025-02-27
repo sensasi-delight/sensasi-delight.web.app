20d18316c08aae03925cb2383ea4ceb5b8a35cf5
Zain🍕 <zainadam.id@gmail.com>
2025-02-27 18:36:24 +0800

    Add new quotes

--- src/constants/data/quotes.ts
+++ src/constants/data/quotes.ts
@@ -3,6 +3,14 @@ const quotes = [
         quote: 'The only way to do great work is to love what you do.',
         author: 'Steve Jobs',
     },
+    {
+        quote: 'The only thing that never changes is change itself.',
+        author: 'D.B. Coulson',
+    },
+    {
+        quote: 'Yippee Ki-Yay, Mr. Falcon.',
+        author: 'John McClane from Die Hard 2 (1990)[TV Version]',
+    },
 ]
 
 export default quotes
