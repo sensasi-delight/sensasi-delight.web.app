02471b0223ff6b889f9cf0cf9a1a58e9fbe328a7
Zain🍕 <zainadam.id@gmail.com>
2025-03-09 00:57:43 +0800

    Remove `globals` deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -46,7 +45,6 @@ export default tseslint.config(
         files: ['**/*.{ts,tsx}'],
         languageOptions: {
             ecmaVersion: 2020,
-            globals: globals.browser,
         },
     },
 
