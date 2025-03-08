f82224895482210c0258d86bc70584d88672d882
Zain🍕 <zainadam.id@gmail.com>
2025-03-09 00:54:09 +0800

    Refactor `<StarBackground />`

--- src/components/layout/_components/star-background.tsx
+++ src/components/layout/_components/star-background.tsx
@@ -1,11 +1,11 @@
-import '@/assets/css/star-field.css'
+import style from './star-background.module.css'
 
 export default function StarBackground() {
     return (
-        <div className="star-field">
-            <div className="layer"></div>
-            <div className="layer"></div>
-            <div className="layer"></div>
+        <div className={style['star-field']}>
+            <div className={style.layer}></div>
+            <div className={style.layer}></div>
+            <div className={style.layer}></div>
         </div>
     )
 }
